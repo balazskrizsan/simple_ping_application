de1ecb89f82670def6f18d91831dd0ba7b8984ba
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-11-25 23:23:16 +0100

    TaskRunRepositoryTest added

---
+++ src/test/java/unit/com/kbalazsworkds/repositories/TaskRunRepositoryTest.java
@@ -0,0 +1,76 @@
+package unit.com.kbalazsworkds.repositories;
+
+import com.kbalazsworkds.repositories.TaskRunRepository;
+import org.junit.jupiter.api.Test;
+
+import static com.kbalazsworkds.enums.RunTypeEnum.ICMP_PING;
+import static com.kbalazsworkds.enums.RunTypeEnum.TCP_PING;
+import static com.kbalazsworkds.enums.RunTypeEnum.TRACEROUTE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class TaskRunRepositoryTest
+{
+    @Test
+    void isRunning_stateCheck_multipleData()
+    {
+        // Arrange
+        TaskRunRepository taskRunRepository = new TaskRunRepository();
+
+        taskRunRepository.setRunning(ICMP_PING, "host1");
+        taskRunRepository.setRunning(ICMP_PING, "host2");
+        taskRunRepository.setRunning(ICMP_PING, "host4");
+        taskRunRepository.setRunning(TCP_PING, "host4");
+
+        // Act
+        boolean actual1 = taskRunRepository.isRunning(ICMP_PING, "host1");
+        boolean actual2 = taskRunRepository.isRunning(ICMP_PING, "host2");
+        boolean actual3 = taskRunRepository.isRunning(ICMP_PING, "host3");
+        boolean actual41 = taskRunRepository.isRunning(ICMP_PING, "host4");
+        boolean actual42 = taskRunRepository.isRunning(TCP_PING, "host4");
+        boolean actual43 = taskRunRepository.isRunning(TRACEROUTE, "host4");
+
+        // Assert
+        assertAll(
+            () -> assertThat(actual1).isTrue(),
+            () -> assertThat(actual2).isTrue(),
+            () -> assertThat(actual3).isFalse(),
+            () -> assertThat(actual41).isTrue(),
+            () -> assertThat(actual42).isTrue(),
+            () -> assertThat(actual43).isFalse()
+        );
+    }
+
+    @Test
+    void setRunning_setState_finishRemoveState()
+    {
+        // Arrange
+        TaskRunRepository taskRunRepository = new TaskRunRepository();
+
+        taskRunRepository.setRunning(ICMP_PING, "host1");
+        taskRunRepository.setRunning(ICMP_PING, "host2");
+        taskRunRepository.setRunning(ICMP_PING, "host4");
+        taskRunRepository.setRunning(TCP_PING, "host4");
+
+        taskRunRepository.finish(ICMP_PING, "host2");
+        taskRunRepository.finish(ICMP_PING, "host4");
+
+        // Act
+        boolean actual1 = taskRunRepository.isRunning(ICMP_PING, "host1");
+        boolean actual2 = taskRunRepository.isRunning(ICMP_PING, "host2");
+        boolean actual3 = taskRunRepository.isRunning(ICMP_PING, "host3");
+        boolean actual41 = taskRunRepository.isRunning(ICMP_PING, "host4");
+        boolean actual42 = taskRunRepository.isRunning(TCP_PING, "host4");
+        boolean actual43 = taskRunRepository.isRunning(TRACEROUTE, "host4");
+
+        // Assert
+        assertAll(
+            () -> assertThat(actual1).isTrue(),
+            () -> assertThat(actual2).isFalse(),
+            () -> assertThat(actual3).isFalse(),
+            () -> assertThat(actual41).isFalse(),
+            () -> assertThat(actual42).isTrue(),
+            () -> assertThat(actual43).isFalse()
+        );
+    }
+}
