9b8f5b0fff76e8dfff673a2830b7339395e3866b
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-11-26 00:13:49 +0100

    Report entity with proper response key

--- src/main/java/com/kbalazsworkds/entities/Report.java
+++ src/main/java/com/kbalazsworkds/entities/Report.java
@@ -1,11 +1,13 @@
 package com.kbalazsworkds.entities;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.NonNull;
 
 public record Report(
-    @NonNull String host,
-    @NonNull String icmpPing,
-    @NonNull String tcpPing
+    @NonNull @SerializedName("host") String host,
+    @NonNull @SerializedName("icmp_ping") String icmpPing,
+    @NonNull @SerializedName("tcp_ping") String tcpPing,
+    @NonNull @SerializedName("trace") String trace
 )
 {
 }
